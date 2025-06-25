d9c217bb52cf79ef52459d61e370e9d6bfe30e2e
Harsh Kumar <harshkumar@Harshs-MacBook-Pro.local>
2025-06-26 01:54:11 +0530

    added changes into py file

--- fetch_crypto.py
+++ fetch_crypto.py
@@ -1,49 +1,35 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import os
-from time import time
-from time import sleep
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+from datetime import datetime
 
 # DB credentials
-
-db_url = os.getenv( 'SUPA_URL' )
-
-# Create engine
+db_url = os.getenv('SUPA_URL')
 engine = create_engine(db_url)
 
 def automation():
-    from requests import Request, Session
-    from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-    import json
-    
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-    parameters = {
-      'start':'1',
-      'limit':'20',
-      'convert':'USD'
-    }
+    parameters = {'start': '1', 'limit': '20', 'convert': 'USD'}
     headers = {
-      'Accepts': 'application/json',
-      'X-CMC_PRO_API_KEY': os.getenv( 'API_KEY' ),
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': os.getenv('API_KEY'),
     }
-    
+
     session = Session()
     session.headers.update(headers)
-    
+
     try:
-      response = session.get(url, params=parameters)
-      data = json.loads(response.text)
-      #print(data)
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
     except (ConnectionError, Timeout, TooManyRedirects) as e:
-      print(e)
-    
+        print(e)
+        return
+
     df = pd.json_normalize(data['data'])
     df['timestamp'] = pd.to_datetime('now')
     df.to_sql('crypto_table', engine, if_exists='append', index=False)
 
-
-for i in range (300):
-    automation()
-    print('queue:',i)
-    sleep(120)
-exit()
+automation()
